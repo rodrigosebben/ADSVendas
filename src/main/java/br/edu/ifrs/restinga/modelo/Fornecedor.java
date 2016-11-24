5d5a1622a1f7d74f04d99babe722531c33b1b0b0
MORGANAANA <mgrosa@restinga.ifrs.edu.br>
2016-11-23 23:51:54 -0200

    inserindo Fornecedores FALTA:Modificar beans, models,e persistencia Arrumar dados no banco Layout e aplicar materialize

---
+++ src/main/java/br/edu/ifrs/restinga/modelo/Fornecedor.java
@@ -0,0 +1,149 @@
+//package br.edu.ifrs.restinga.modelo;
+//
+///**
+// *
+// * @author Morgana
+// */
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.Id;
+//import javax.persistence.Table;
+//
+//@Entity
+//@Table(name="empresa")
+//public class Empresa 
+//{
+//    @Id
+//    @GeneratedValue
+//    private int id;
+//    private String nomeEmpresa;
+//    private String cnpj;
+//    private String inscricaoEstadual;
+//    private String responsavel;
+//    private String email;
+//    private String site;
+//    private String endereco;
+//    private String compl;
+//    private String bairro;
+//    private String cidade;
+//    private String uf;
+//    private String telefone;
+//    private String celular;
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getNomeEmpresa() {
+//        return nomeEmpresa;
+//    }
+//
+//    public void setNomeEmpresa(String nomeEmpresa) {
+//        this.nomeEmpresa = nomeEmpresa;
+//    }
+//
+//    public String getCnpj() {
+//        return cnpj;
+//    }
+//
+//    public void setCnpj(String cnpj) {
+//        this.cnpj = cnpj;
+//    }
+//
+//    public String getInscricaoEstadual() {
+//        return inscricaoEstadual;
+//    }
+//
+//    public void setInscricaoEstadual(String inscricaoEstadual) {
+//        this.inscricaoEstadual = inscricaoEstadual;
+//    }
+//
+//    public String getResponsavel() {
+//        return responsavel;
+//    }
+//
+//    public void setResponsavel(String responsavel) {
+//        this.responsavel = responsavel;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public String getSite() {
+//        return site;
+//    }
+//
+//    public void setSite(String site) {
+//        this.site = site;
+//    }
+//
+//    public String getEndereco() {
+//        return endereco;
+//    }
+//
+//    public void setEndereco(String endereco) {
+//        this.endereco = endereco;
+//    }
+//
+//    public String getCompl() {
+//        return compl;
+//    }
+//
+//    public void setCompl(String compl) {
+//        this.compl = compl;
+//    }
+//
+//    public String getBairro() {
+//        return bairro;
+//    }
+//
+//    public void setBairro(String bairro) {
+//        this.bairro = bairro;
+//    }
+//
+//    public String getCidade() {
+//        return cidade;
+//    }
+//
+//    public void setCidade(String cidade) {
+//        this.cidade = cidade;
+//    }
+//
+//    public String getUf() {
+//        return uf;
+//    }
+//
+//    public void setUf(String uf) {
+//        this.uf = uf;
+//    }
+//
+//    public String getTelefone() {
+//        return telefone;
+//    }
+//
+//    public void setTelefone(String telefone) {
+//        this.telefone = telefone;
+//    }
+//
+//    public String getCelular() {
+//        return celular;
+//    }
+//
+//    public void setCelular(String celular) {
+//        this.celular = celular;
+//    }
+//
+//    public boolean ehNovo() {
+//        return id == 0;
+//    }
+//    
+//}
